07a6ff5504ea51f464e0208af4cc2d94b3aa79be
Neeharika325 <neeharikavaranasi2002@gmail.com>
2025-11-30 22:07:24 +0530

    Q1.js changes done

--- DOM/Q1.js
+++ DOM/Q1.js
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>DOM Practice</title>
-    </head>
+    <html> 
     <body>
         <h1 id="main-heading">Original Heading</h1>
         <p>This is paragraph 1.</p>
@@ -14,9 +9,15 @@
             This is container div.
         </div>
         <script>
-            const heading = document.getElementById(`main-heading`);
+            const heading = document.getElementById("main-heading");
             heading.textContent="Welcome to DOM World!";
-            const
+
+            const paragraphs=document.getElementsByTagName("p");
+            for(let p of paragraphs) {
+                p.style.color="blue";
+            }
+            const containerDiv=document.querySelector(".container");
+            containerDiv.style.backgroundColor="yellow";
         </script>
     </body>
 </html>
