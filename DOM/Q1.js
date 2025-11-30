880c984d46298b05a60e76c9bda84b8c9af86f7c
Neeharika325 <neeharikavaranasi2002@gmail.com>
2025-11-30 21:46:34 +0530

    Q1.js done

---
+++ DOM/Q1.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>DOM Practice</title>
+    </head>
+    <body>
+        <h1 id="main-heading">Original Heading</h1>
+        <p>This is paragraph 1.</p>
+        <p>This is paragraph 2.</p>
+        <p>This is paragraph 3.</p>
+
+        <div class="container">
+            This is container div.
+        </div>
+        <script>
+            const heading = document.getElementById(`main-heading`);
+            heading.textContent="Welcome to DOM World!";
+            const
+        </script>
+    </body>
+</html>
